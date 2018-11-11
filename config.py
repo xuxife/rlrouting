e36e5b5b92705cb37e523cbace74095f773ca67a
xuxife <xuxife@gmail.com>
2018-11-11 12:28:56 +0800

    add LearnRateP

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 Lambda         = 10   # packets per second
 Timeout        = 0.5  # packets living over Timeout would be drop
 TransTime      = 0.1  # transmission delay
-LearnRate      = 0.1
+LearnRateQ     = 0.1
+LearnRateP     = 0.1
 BandwidthLimit = 3    # the maximum capacity of a connection
