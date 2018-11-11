3924f249edd2c869311d97850c7659eac88cd9c7
xuxife <xuxife@gmail.com>
2018-11-11 15:18:50 +0800

    add get_reward and revise learn

--- hybrid.py
+++ hybrid.py
@@ -2,45 +2,53 @@ import math
 import random
 from collections import OrderedDict
 
-from config import LearnRate
+from config import LearnRateQ, LearnRateP
 
 class HybridQ:
     def __init__(self, network, initQ, initP):
-        self.Qtable, self.Ptable = OrderedDict(), OrderedDict()
+        self.Qtable, self.Theta = {}, {}
         for node, neibors in network.links.items():
             self.Qtable[node] = {}
-            self.Ptable[node] = {}
+            self.Theta[node] = {}
             for dest_node in network.links:
                 if dest_node == node:
                     continue
-                self.Qtable[node][dest_node] = {k: initQ for k in neibors}
-                self.Ptable[node][dest_node] = {k: initP for k in neibors}
+                self.Qtable[node][dest_node] = OrderedDict({k: initQ for k in neibors})
+                self.Theta[node][dest_node] = OrderedDict({k: initP for k in neibors})
     
     def choose(self, source, dest):
         """ choose returns the choice following weighted random sample, and the Q score of the choice
         """
-        population, weight = zip(*[(k, v) for k, v in self.exp_p_score.items()])
+        population, weight = zip(*[(k, v) for k, v in self.exp_theta(source, dest).items()])
         choice = random.choices(population, weight)
-        return choice, self.Qtable[choice]
+        return choice, self.Qtable[source][dest][choice]
 
-    @property
-    def exp_p_score(self, source, dest):
-        return OrderedDict({k: math.exp(v) for k, v in self.Ptable[source][dest].items()})
+    def exp_theta(self, source, dest):
+        return OrderedDict({k: math.exp(v) for k, v in self.Theta[source][dest].items()})
     
-    def learn(self, reward, lrq=LearnRate, lrp=LearnRate):
-        q, t = reward.queue_time, reward.trans_time
-        source, dest = reward.source, reward.dest
-        next_score = reward.score
-        old_Q_score = self.Qtable[source][dest]
-        self.Qtable[source][dest] += lrq * (q + t + next_score - old_Q_score)
-        _, min_score = self.choose(source, dest)
-        for neibor in self.Ptable[source][dest]:
-            self.Ptable[source][dest][neibor] -= lrp * (q + t + next_score - min_score) * self._gradient(source, dest, reward.action, neibor)
+    def min_q_score(self, source, dest):
+        return min(self.Qtable[source][dest].values())
 
-    def _gradient(self, source, dest, action, theta):
-        exp_p_score = self.exp_p_score
-        exp_sum = sum(exp_p_score.values())
+    def get_reward(self, source, dest, action):
+        agent_info = {}
+        agent_info['action_min'] = self.min_q_score(action, dest)
+        agent_info['source_min'] = self.min_q_score(source, dest)
+        return agent_info
+
+    def learn(self, reward, lrq=LearnRateQ, lrp=LearnRateP):
+        q, t = reward.queue_time, reward.trans_time
+        source, dest, action = reward.source, reward.dest, reward.action
+        action_min = reward.agent_info['action_min']
+        source_min = reward.agent_info['source_min']
+        old_Q_score = self.Qtable[source][dest][action]
+        self.Qtable[source][dest][action] += lrq * (q + t + action_min - old_Q_score)
+        for neibor in self.Theta[source][dest]:
+            self.Ptable[source][dest][neibor] -= lrp * (q + t + action_min - source_min) * self.gradient(source, dest, action, neibor)
+
+    def gradient(self, source, dest, action, theta):
+        exp_theta = self.exp_theta(souce, dest)
+        exp_sum = sum(exp_theta.values())
         if theta == action:
-            return 1 - exp_p_score[theta]/exp_sum
-        return - exp_p_score[theta]/exp_sum
+            return 1 - exp_theta[theta]/exp_sum
+        return - exp_theta[theta]/exp_sum
     
