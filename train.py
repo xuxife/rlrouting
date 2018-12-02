a8b4abf8efec391db5a2a0949cc68793c10eb6ac
xuxife <xuxife@gmail.com>
2018-12-02 20:59:24 +0800

    train: add pickle load saved agent

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import logging
 import numpy as np
+import pickle
 
 from env import *
 from config import *
@@ -8,13 +9,12 @@ from qroute import Qroute
 from hybrid import HybridQ
 from multi_agent import MaHybridQ
 
-Duration = 1
 np.random.seed(1)
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+# logger = logging.getLogger()
+# logger.setLevel(logging.DEBUG)
 
 File = "6x6.net"
-nw   = Network(File)
+nw = Network(File)
 
 # =============
 # agent = Shortest(nw)
@@ -22,10 +22,25 @@ agent = Qroute(nw)
 # agent = HybridQ(nw)
 # agent = MaHybridQ(nw)
 # =============
+# agentFile = "0.2/qroute3w0.3.pkl"
+agentFile = "1.5/qroute.pkl"
+with open(agentFile, "rb") as f:
+    agent = pickle.load(f)
+# =============
 
 for node in nw.nodes.values():
     node.agent = agent
 
-while nw.clock < Duration:
-    reward = nw.step()
-    agent.learn(reward)
+route_time = []
+for i in range(10000):
+    r = nw.step(lambd=1.8)
+    if r is not None:
+        agent.learn(r, lr=0.3)
+        # agent.learn(r, lrq=0.01, lrp=0.01)
+    route_time.append(nw.ave_route_time)
+
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.DataFrame(route_time)
+df.plot()
+plt.show()
