78b76ede41c20b8f330a017e13548cb10e9ce829
xuxife <xuxife@gmail.com>
2019-04-16 20:26:48 +0800

    move dijkstra function out & reform with np :wrench:

--- shortest.py
+++ shortest.py
@@ -6,22 +6,23 @@ class Shortest:
     """ Shortest agent determine the action based on the shortest path distance. """
 
     def __init__(self, network):
-        node_num = len(network.nodes)
-        self.distance = np.zeros((node_num, node_num), dtype=np.int)
+        self.nw = network
+        node_num = len(self.nw.nodes)
+        self.distance = np.full((node_num, node_num), np.inf)
+        np.fill_diagonal(self.distance, 0)
         self.choice = np.zeros((node_num, node_num), dtype=np.int)
         for node, neibors in network.links.items():
             for neibor in neibors:
                 self.distance[node][neibor] = 1
                 self.choice[node][neibor] = neibor
-            for dest_node in network.links.keys():
-                if dest_node != node and dest_node not in neibors:
-                    self.distance[node][dest_node] = len(
-                        network.links) + 1  # large enough
+        self.dijkstra()
+
+    def dijkstra(self):
         changing = True
         while changing:
             changing = False
-            for source, neibors in network.links.items():
-                for dest in network.nodes:
+            for source, neibors in self.nw.links.items():
+                for dest in self.nw.nodes:
                     for neibor in neibors:
                         if self.distance[source][dest] > self.distance[neibor][dest] + 1:
                             self.distance[source][dest] = self.distance[neibor][dest] + 1
