018add438f342f7f3ea81f42f1cd012de08956ba
xuxife <xuxife@gmail.com>
2019-04-19 19:54:59 +0800

    :art: Qroute inherit from base_policy & restructure Qtable

--- qroute.py
+++ qroute.py
@@ -4,43 +4,38 @@ from base_policy import *
 from config import *
 
 
-class Qroute:
+class Qroute(Policy):
     """ Qroute use Q routing as policy. 
 
     Attritubes:
         Qtable : Stores the Q scores of all nodes.
     """
+    attrs = ['Qtable']
 
     def __init__(self, network, initQ=InitQ):
         self.links = network.links
-        node_num = len(self.links)
-        self.neibor_num = np.array([len(self.links[i])
-                                    for i in range(len(self.links))])
-        self.Qtable = InitQ * \
-            np.ones((node_num, node_num, self.neibor_num.max()))
+        self.Qtable = {source:
+                       {dest: InitQ * np.ones(len(neighbors))
+                        for dest in self.links.keys()}
+                       for source, neighbors in self.links.items()}
 
     def choose(self, source, dest):
-        scores = self.Qtable[source][dest][:self.neibor_num[source]]
-        choice = np.random.choice(np.argwhere(
-            scores == scores.max()).flatten())
+        scores = self.Qtable[source][dest]
+        score_max = scores.max()
+        choice = np.random.choice(np.argwhere(scores == score_max).flatten())
         return self.links[source][choice]
 
-    def get_reward(self, source, dest, action):
-        return {'action_max': self.Qtable[action][dest][:self.neibor_num[action]].max()}
+    def get_reward(self, source, action, packet):
+        return {'max_Q_y': self.Qtable[action][packet.dest].max()}
 
-    def learn(self, reward_list, lrq=LearnRateQ, lrp=0):
-        for reward in reward_list:
+    def learn(self, rewards, lr={'q': LearnRateQ}):
+        for reward in rewards:
             q, t = reward.queue_time, reward.trans_time
             source, dest, action = reward.source, reward.dest, reward.action
-            action_max = reward.agent_info['action_max']
+            action_max = reward.agent_info['max_Q_y']
             action_index = self.links[source].index(action)
             old_score = self.Qtable[source][dest][action_index]
-            self.Qtable[source][dest][action_index] += lrq * \
+            self.Qtable[source][dest][action_index] += lr['q'] * \
                 (-q-t + action_max - old_score)
 
-    def store(self, filename):
-        with open(filename, "wb") as f:
-            pickle.dump(self, f)
 
-    def drop_penalty(self, event, penalty=DropPenalty):
-        pass
