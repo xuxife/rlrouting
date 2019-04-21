4039f8892d4118fd354e5945b8945d759752195e
xuxife <xuxife@gmail.com>
2019-04-21 14:21:18 +0800

    :construction: restructure policygradient

--- hybrid.py
+++ hybrid.py
@@ -1,68 +1,76 @@
-import math
-import pickle
 import numpy as np
 
+from qroute import *
+from base_policy import *
 from config import *
 
 
-class HybridQ:
-    def __init__(self, network, initQ=InitQ, initP=InitP, discount=Discount):
-        self.discount = discount
-        self.nw, self.links = network, network.links
-        node_num = len(self.links)
-        self.neibor_num = np.array([len(self.links[i])
-                                    for i in self.links.keys()])
-        self.Qtable = InitQ * \
-            np.ones((node_num, node_num, self.neibor_num.max()))
-        self.Theta = InitP * \
-            np.ones((node_num, node_num, self.neibor_num.max()))
+class PolicyGradient(Policy):
+    attrs = Policy.attrs + ['Theta', 'discount']
+
+    def __init__(self, network, initP=InitP):
+        self.links = network.links
+        self.Theta = {source:
+                      np.ones((len(self.links), len(neighbors))) * initP
+                      for source, neighbors in self.links.items()}
 
     def choose(self, source, dest):
         """ choose returns the choice following weighted random sample """
-        theta = self.Theta[source][dest][:self.neibor_num[source]]
-        e_theta = np.exp(theta)
+        e_theta = np.exp(self.Theta[source][dest])
         choice = np.random.choice(
-            self.links[source], p=e_theta/e_theta.sum(axis=0))
+            self.links[source], p=e_theta/e_theta.sum())
         return choice
 
-    def get_reward(self, source, dest, action):
-        return {
-            'action_max': self.Qtable[action][dest][:self.neibor_num[action]].max(),
-            'source_max': self.Qtable[source][dest][:self.neibor_num[source]].max(),
-        }
-
-    def learn(self, reward_list, lrq=LearnRateQ, lrp=LearnRateP):
-        for reward in reward_list:
-            q, t = reward.queue_time, reward.trans_time
-            source, dest, action = reward.source, reward.dest, reward.action
-            action_max = reward.agent_info['action_max']
-            source_max = reward.agent_info['source_max']
-            action_index = self.links[source].index(action)
-            old_Q_score = self.Qtable[source][dest][action_index]
-            self.Qtable[source][dest][action_index] += lrq * \
-                (-q-t + self.discount*action_max - old_Q_score)
-            delta = lrp *\
-                (-q-t + self.discount*action_max - source_max) * \
-                self.gradient(source, dest, action)
-            self.Theta[source][dest][:self.neibor_num[source]] += delta
-        np.clip(self.Theta, -2, 2)
-
-    def gradient(self, source, dest, action):
+    def _gradient(self, source, dest, action):
         """ gradient returns a vector with length of neibors of source """
-        theta = self.Theta[source][dest][:self.neibor_num[source]]
-        e_theta = np.exp(theta)
-        gradient = - e_theta/e_theta.sum(axis=0)
-        for i, n in enumerate(self.links[source]):
-            if n == action:
-                gradient[i] += 1
+        e_theta = np.exp(self.Theta[source][dest])
+        gradient = - e_theta/e_theta.sum()
+        gradient[self.links[source].index(action)] += 1
         return gradient
 
-    def store(self, filename):
-        with open(filename, "wb") as f:
-            pickle.dump(self, f)
 
-    def drop_penalty(self, event, penalty=DropPenalty):
+class HybridQ(Qroute, PolicyGradient):
+    attrs = Qroute.attrs + PolicyGradient.attrs
+
+    def __init__(self, network, initQ=InitQ, initP=InitP, discount=Discount):
+        self.discount = discount
+        Qroute.__init__(self, network, initQ)
+        PolicyGradient.__init__(self, network, initP)
+
+    def get_reward(self, source, action, packet):
+        return {
+            'max_Q_y': self.Qtable[action][packet.dest].max(),
+            'max_Q_x_d': self.Qtable[source][packet.dest].max(),
+        }
+
+    def learn(self, rewards, lr={'q': LearnRateQ, 'p': LearnRateP}):
+        for reward in filter(lambda r: r.action != r.dest, rewards):
+            source, dest, action = reward.source, reward.dest, reward.action
+            agent_info = reward.agent_info
+            action_index = self.links[source].index(action)
+            old_score = self.Qtable[source][dest][action_index]
+            self.Qtable[source][dest][action_index] += lr['q'] * \
+                (-agent_info['q_y'] + self.discount *
+                 agent_info['max_Q_y'] - old_score)
+            self.Theta[source][dest] += lr['p'] * \
+                (-agent_info['q_y'] + self.discount*agent_info['max_Q_y'] - agent_info['max_Q_x_d']) * \
+                self._gradient(source, dest, action)
+        for theta in self.Theta.values():
+            np.clip(theta, -2, 2)
+
+
+class HybridCDRQ(CDRQ, PolicyGradient):
+    attrs = CDRQ.attrs + PolicyGradient.attrs
+
+    def __init__(self, network, initQ=InitQ, initP=InitP, discount=Discount):
+        self.discount = discount
+        CDRQ.__init__(self, network, initQ=initQ)
+        PolicyGradient.__init__(self, network, initP)
+
+    def get_reward(self, source, action, packet):
+        info = CDRQ.get_reward(self, source, action, packet)
+        info['max_Q_x_d'] = self.Qtable[source][packet.dest].max()
+        return info
+
+    def learn(self, rewards, lr={'f': 0.85, 'b': 0.95, 'q': LearnRateQ, 'p': LearnRateP}):
         pass
-
-    def __repr__(self):
-        return "<HybridQ discount:{}>".format(self.discount)
