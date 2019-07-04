26b7f9dfa76ecaeafd5ad9eae11717e7b21e4d94
xuxife <xuxife@gmail.com>
2019-07-04 13:37:36 -0700

    remove debug print & update doc for Shortest

--- shortest.py
+++ shortest.py
@@ -4,7 +4,13 @@ from base_policy import Policy
 
 
 class Shortest(Policy):
-    """ Shortest agent determine the action based on the shortest path distance. """
+    """ Shortest agent determine the action based on Dijkstra algorithm. 
+
+    Attributes:
+        distance (np.array(float64, (nodes, nodes))): stores the distance between nodes
+        mask (np.array(bool, (nodes, nodes))): indicates `non-neighbors`, who are assumed infinity distance before dijkstra
+        choice (Dict[Int, np.array(bool, (nodes, neighbors))]): choice[a][b, :] indicates Node a to Node b which (multiple) neighbor(s) can be chosen
+    """
     attrs = Policy.attrs | set(['distance', 'choice', 'mask'])
 
     def __init__(self, network):
@@ -18,17 +24,15 @@ class Shortest(Policy):
             self.mask[x, x] = False
             for y in neighbors:
                 self.distance[x, y] = 1
-                self.choice[x][y][self.action_idx[x][y]] = True
+                self.choice[x][y, self.action_idx[x][y]] = True
                 self.mask[x, y] = False
         self.unit = lambda x: 1  # regard unit distance as 1
         self._calc_distance()
 
     def choose(self, source, dest, random=False):
         """ Return the action with shortest distance and the distance """
-        if random:
-            return np.random.choice(self.links[source][self.choice[source][dest]])
-        else:
-            return self.links[source][self.choice[source][dest]][0]
+        choices = self.links[source][self.choice[source][dest]]
+        return np.random.choice(choices) if random else choices[0]
 
     def _calc_distance(self):
         self.distance[self.mask] = np.inf
@@ -36,21 +40,18 @@ class Shortest(Policy):
         while changing:
             changing = False
             for x in self.links.keys():
-                if x == 19:
-                    print(self.distance[x])
                 for y in self.links[x]:
                     new_dis = self.distance[y] + self.unit(y)
                     greater = self.distance[x] > new_dis
-                    if x == 19:
-                        print(y, new_dis)
                     if greater.any():
                         self.distance[x][greater] = new_dis[greater]
                         self.choice[x][greater, :].fill(False)
                         self.choice[x][greater, self.action_idx[x][y]] = True
                         changing = True
-                    else:
-                        self.choice[x][self.distance[x] ==
-                                       new_dis, self.action_idx[x][y]] = True
+                    equal = (~np.isinf(self.distance[x])) & (
+                        self.distance[x] == new_dis)
+                    if equal[self.mask[x]].any():
+                        self.choice[x][equal, self.action_idx[x][y]] = True
 
 
 class GlobalRoute(Shortest):
