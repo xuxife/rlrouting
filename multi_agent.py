792f88d246f4f6354daa64926b5d79128700206c
xuxife <xuxife@gmail.com>
2019-09-15 21:34:20 +0800

    fix bug!! line 45 self.Trace[i] <= i is not defined in for loop

--- multi_agent.py
+++ multi_agent.py
@@ -1,52 +1,52 @@
 import numpy as np
 
-from hybrid import *
+from hybrid import HybridQ
 
 
 class MaHybridQ(HybridQ):
     """ Multi-agent Hybrid Q routing with Eligibility Traces """
+    attrs = HybridQ.attrs | set(['Trace'])
 
     def __init__(self, network, initQ=0, initP=0, discount=0.99, discount_trace=0.6):
-        super().__init__(network, initQ, initP)
-        self.Trace = {}
-        self.discount = discount
+        super().__init__(network, initQ=initQ, initP=initP, discount=discount)
         self.discount_trace = discount_trace
         self.reward_shape = 0
-        self.Trace = np.zeros(self.Qtable.shape)
+        self.Trace = {k: np.zeros_like(v) for k, v in self.Theta.items()}
 
-    def learn(self, reward_list, lrq=0.1, lrp=0.1):
-        assert isinstance(
-            reward_list, list), "input should be a list of rewards"
-        num_rewards = len(reward_list)
+    def learn(self, rewards, lr={'q': 0.1, 'p': 0.1}):
+        num_rewards = len(rewards)
         q, t = np.zeros(num_rewards), np.zeros(num_rewards)
-        source, dest, action = np.zeros(num_rewards, dtype=np.int), np.zeros(
+        x, dest, y_idx = np.zeros(num_rewards, dtype=np.int), np.zeros(
             num_rewards, dtype=np.int), np.zeros(num_rewards, dtype=np.int)
-        action_max, source_max = np.zeros(num_rewards), np.zeros(num_rewards)
-        for i, reward in enumerate(reward_list):
-            q[i], t[i] = reward.queue_time, reward.trans_time
-            source[i], dest[i], action[i] = reward.source, reward.dest, reward.action
-            action_max[i] = reward.agent_info['action_max']
-            source_max[i] = reward.agent_info['source_max']
+        max_Q_y, max_Q_x_d = np.zeros(num_rewards), np.zeros(num_rewards)
+        for i, reward in enumerate(rewards):
+            x[i], dest[i] = reward.source, reward.dest
+            y_idx[i] = self.action_idx[x[i]][reward.action]
+            info = reward.agent_info
+            q[i], t[i] = -info['q_y']-info['t_y']
+            max_Q_y[i] = info['max_Q_y']
+            max_Q_x_d[i] = info['max_Q_x_d']
 
-        r = - q - t  # r_t = -(q+t) + r_shaping
+        r = - q - t
         delta = r.sum() + self.reward_shape + self.discount * \
-            action_max.sum() - source_max.sum()
+            max_Q_y.sum() - max_Q_x_d.sum()
         self.reward_shape = 0
-        # update Eligibility Trace
+        # update Eligibility Trac
         self.Trace *= self.discount_trace
         for i in range(num_rewards):
-            self.Trace[source[i]][dest[i]][:self.neibor_num[source[i]]
-                                           ] += self.gradient(source[i], dest[i], action[i])
+            self.Trace[x[i]][dest[i]] += \
+                self._gradient(x[i], dest[i], y_idx[i])
             # update Q table
-            action_index = self.links[source[i]].index(action[i])
-            old_Q_score = self.Qtable[source[i]][dest[i]][action_index]
-            self.Qtable[source[i]][dest[i]][action_index] += lrq * \
-                (r[i] + self.discount*action_max[i] - old_Q_score)
+            old_Q_score = self.Qtable[x[i]][dest[i]][y_idx[i]]
+            self.Qtable[x[i]][dest[i]][y_idx[i]] += lr['q'] * \
+                (r[i] + self.discount*max_Q_y[i] - old_Q_score)
         # Update Theta
-        self.Theta += lrp * delta * self.Trace
+        for x, theta in self.Theta.items():
+            theta += lr['p'] * delta * self.Trace[x]
 
     def __repr__(self):
         return "<MultiAgent discount:{} discount_trace:{}>".format(self.discount, self.discount_trace)
 
     def clean_trace(self):
-        self.Trace = np.zeros(self.Qtable.shape)
+        for trace in self.Trace.values():
+            trace.fill(0.0)
