62a0e3da9505574e27646888377c85a2b310c2fe
xuxife <xuxife@gmail.com>
2019-09-15 21:33:53 +0800

    extract update_qtable & update_theta function

--- hybrid.py
+++ hybrid.py
@@ -6,39 +6,43 @@ from qroute import *
 class PolicyGradient(Policy):
     attrs = Policy.attrs | set(['Theta', 'discount'])
 
-    def __init__(self, network, initP=0):
+    def __init__(self, network, initP=0, add_entropy=False, discount=0.9):
         super().__init__(network)
-        self.Theta = {source:
-                      np.random.normal(
-                          initP, 1, (len(self.links), len(neighbors)))
-                      for source, neighbors in self.links.items()}
+        self.add_entropy = add_entropy
+        self.discount = discount
+        self.Theta = {x: np.full((len(self.links), len(ys)), initP)
+                      for x, ys in self.links.items()}
 
     def _softmax(self, source, dest):
-        e_theta = np.exp(self.Theta[source][dest])
+        e_theta = self.Theta[source][dest].exp()
         return e_theta/e_theta.sum()
 
-    def choose(self, source, dest):
+    def choose(self, source, dest, prob=None):
         """ choose returns the choice following weighted random sample """
-        choice = np.random.choice(
-            self.links[source], p=self._softmax(source, dest))
-        return choice
+        return np.random.choice(self.links[source], p=(
+            self._softmax(source, dest) if prob is None else prob))
 
-    def _gradient(self, source, dest, action_idx):
+    def _gradient(self, source, dest, action_idx, softmax=None):
         """ gradient returns a vector with length of neighbors of source """
-        gradient = -self._softmax(source, dest)
+        gradient = -self._softmax(source, dest) \
+            if softmax is None else (-softmax)
         gradient[action_idx] += 1
         return gradient
+
+    def _update_theta(self, r, x, y, dest, max_Q_y, max_Q_x_d, lrp, softmax=None):
+        gradient = self._gradient(
+            x, dest, self.action_idx[x][y], softmax=softmax)
+        self.Theta[x][dest] += lrp * gradient * \
+            (r + self.discount * max_Q_y - max_Q_x_d)
 
 
 class HybridQ(PolicyGradient, Qroute):
     attrs = Qroute.attrs | PolicyGradient.attrs
 
-    def __init__(self, network, add_entropy=True, initQ=0, initP=0, discount=0.99):
-        self.discount = discount
-        self.add_entropy = add_entropy
-        self.constrain = (-2, 2)
-        PolicyGradient.__init__(self, network, initP)
-        Qroute.__init__(self, network, initQ)
+    def __init__(self, network, initQ=0, initP=0, add_entropy=True, discount=0.99):
+        PolicyGradient.__init__(self, network, initP,
+                                add_entropy=add_entropy, discount=discount)
+        Qroute.__init__(self, network, initQ, discount=discount)
 
     def get_info(self, source, action, packet):
         return {
@@ -48,88 +52,84 @@ class HybridQ(PolicyGradient, Qroute):
 
     def learn(self, rewards, lr={'q': 0.1, 'p': 0.1, 'e': 0.1}):
         for reward in rewards:
-            source, dest, action = reward.source, reward.dest, reward.action
-            info = reward.agent_info
-            action_idx = self.action_idx[source][action]
-            softmax = self._softmax(source, dest)
-            r = -info['q_y']-info['t_y']
+            r, info, x, y, dest = self._extract(reward)
+            softmax = self._softmax(x, dest)
             if self.add_entropy:
                 r -= lr['e'] * (softmax*np.log2(softmax)).sum()
-            old_score = self.Qtable[source][dest][action_idx]
-            self.Qtable[source][dest][action_idx] += lr['q'] * \
-                (r + self.discount * info['max_Q_y'] - old_score)
-            gradient = -softmax
-            gradient[action_idx] += 1
-            self.Theta[source][dest] += lr['p'] * gradient * \
-                (r + self.discount * info['max_Q_y'] - info['max_Q_x_d'])
-        for theta in self.Theta.values():
-            theta.clip(*self.constrain, out=theta)
+            self._update_qtable(r, x, y, dest, info['max_Q_y'], lr['q'])
+            self._update_theta(
+                r, x, y, dest, info['max_Q_y'], info['max_Q_x_d'], lr['p'], softmax=softmax)
+
+
+class HybridCQ(PolicyGradient, CQ):
+    attrs = CQ.attrs | PolicyGradient.attrs
+
+    def __init__(self, network, initQ=0, initP=0, decay=0.9, add_entropy=False, discount=0.99):
+        PolicyGradient.__init__(self, network, initP,
+                                add_entropy=add_entropy, discount=discount)
+        CDRQ.__init__(self, network, decay=decay,
+                      initQ=initQ, discount=discount)
+
+    def get_info(self, source, action, packet):
+        z_f, max_Q_f = Qroute.choose(self, action, packet.dest, score=True)
+        return {
+            'max_Q_f': max_Q_f,
+            'C_f': self.confidence[action][packet.dest][z_f],
+            'max_Q_x_d': self.Qtable[source][packet.dest].max(),
+        }
+
+    def learn(self, rewards, lr={'p': 0.1, 'e': 0.1}):
+        for reward in rewards:
+            r_f, info, x, y, dest = self._extract(reward)
+            softmax_f = self._softmax(x, dest)
+            if self.add_entropy:
+                r_f -= lr['e'] * np.log2(softmax_f[self.action_idx[x][y]])
+            self._update_qtable(r_f, x, y, dest, info['C_f'], info['max_Q_f'])
+            self._update_theta(
+                r_f, x, y, dest, info['max_Q_y'], info['max_Q_x_d'], lr['p'], softmax=softmax_f)
+
+        self.confidence_decay()
 
 
 class HybridCDRQ(PolicyGradient, CDRQ):
     attrs = CDRQ.attrs | PolicyGradient.attrs
 
     def __init__(self, network, add_entropy=False, initQ=0, initP=0, decay=0.9, discount=0.99):
-        self.discount = discount
-        self.add_entropy = add_entropy
-        self.constrain = (-2, 2)
-        PolicyGradient.__init__(self, network, initP)
-        CDRQ.__init__(self, network, decay=decay, initQ=initQ)
+        PolicyGradient.__init__(self, network, initP,
+                                add_entropy=add_entropy, discount=discount)
+        CDRQ.__init__(self, network, decay=decay,
+                      initQ=initQ, discount=discount)
 
     def get_info(self, source, action, packet):
-        z_b, max_Q_b = CDRQ.choose(self, source, packet.source, score=True)
-        z_f, max_Q_f = CDRQ.choose(self, action, packet.dest, score=True)
+        w_idx, max_Q_b = Qroute.choose(self, source, packet.source, score=True)
+        z_idx, max_Q_f = Qroute.choose(self, action, packet.dest, score=True)
         return {
             'max_Q_b': max_Q_b,
             'max_Q_f': max_Q_f,
-            'C_b': self.confidence[source][packet.source][z_b],
-            'C_f': self.confidence[action][packet.dest][z_f],
+            'C_b': self.confidence[source][packet.source][w_idx],
+            'C_f': self.confidence[action][packet.dest][z_idx],
             'max_Q_x_d': self.Qtable[source][packet.dest].max(),
             'max_Q_y_s': self.Qtable[action][packet.source].max(),
         }
 
     def learn(self, rewards, lr={'f': 0.85, 'b': 0.95, 'p': 0.1, 'e': 0.1}):
         for reward in rewards:
-            x, y = reward.source, reward.action
-            source, dest = reward.packet.source, reward.packet.dest
-            info = reward.agent_info
-            x_idx, y_idx = self.action_idx[y][x], self.action_idx[x][y]
+            r_f, info, x, y, dest = self._extract(reward)
+            source = reward.packet.source
+            r_b = -info['q_x'] - info['t_x']
             softmax_f = self._softmax(x, dest)
             softmax_b = self._softmax(y, source)
             if self.add_entropy:
-                r_f = -info['q_y'] - lr['e'] * np.log2(softmax_f[y_idx])
-                r_b = -info['q_x'] - lr['e'] * np.log2(softmax_b[x_idx])
-            else:
-                r_f, r_b = -info['q_y'], -info['q_x']
-            if y != dest:
-                old_Q_f = self.Qtable[x][dest][y_idx]
-                eta_f = max(info['C_f'], 1-self.confidence[x][dest][y_idx])
-                self.Qtable[x][dest][y_idx] += lr['f'] * eta_f * \
-                    (r_f + self.discount * info['max_Q_f'] - old_Q_f)
-                self.confidence[x][dest][y_idx] += eta_f * \
-                    (info['C_f']-self.confidence[x][dest][y_idx])
-                self.updated_conf[x][dest][y_idx] = True
-                gradient_f = -softmax_f
-                gradient_f[y_idx] += 1
-                self.Theta[x][dest] += lr['p'] * gradient_f * \
-                    (r_f + self.discount * info['max_Q_f'] - info['max_Q_x_d'])
-            if x != source:
-                old_Q_b = self.Qtable[y][source][x_idx]
-                eta_b = max(info['C_b'], 1-self.confidence[y][source][x_idx])
-                self.Qtable[y][source][x_idx] += lr['b'] * eta_b * \
-                    (r_b + self.discount * info['max_Q_b'] - old_Q_b)
-                self.confidence[y][source][x_idx] += eta_b * \
-                    (info['C_b']-self.confidence[y][source][x_idx])
-                self.updated_conf[y][source][x_idx] = True
-                gradient_b = -softmax_b
-                gradient_b[x_idx] += 1
-                self.Theta[y][source] += lr['p'] * gradient_b * \
-                    (r_b + self.discount * info['max_Q_b'] - info['max_Q_y_s'])
+                r_f -= lr['e'] * np.log2(softmax_f[self.action_idx[x][y]])
+                r_b -= lr['e'] * np.log2(softmax_b[self.action_idx[y][x]])
+            # forward update
+            self._update_qtable(r_f, x, y, dest, info['C_f'], info['max_Q_f'])
+            self._update_theta(
+                r_f, x, y, dest, info['max_Q_f'], info['max_Q_x_f'], lr['p'], softmax=softmax_f)
+            # backward update
+            self._update_qtable(r_b, y, x, source,
+                                info['C_b'], info['max_Q_b'])
+            self._update_theta(
+                r_b, y, x, source, info['max_Q_b'], info['max_Q_y_s'], lr['p'], softmax=softmax_b)
 
-        for node, table in self.updated_conf.items():
-            self.confidence[node][~table] *= self.decay
-            table.fill(False)
-            table[self.links[node]] = np.eye(table.shape[1], dtype=bool)
-
-        for theta in self.Theta.values():
-            theta.clip(*self.constrain, out=theta)
+        self.confidence_decay()
--- qroute.py
+++ qroute.py
@@ -36,20 +36,23 @@ class Qroute(Policy):
         r = -info['q_y'] - info['t_y']
         return r, info, x, y, d
 
+    def _update_qtable(self, r, x, y, d, max_Q_y, lr):
+        y_idx = self.action_idx[x][y]
+        old_score = self.Qtable[x][d][y_idx]
+        self.Qtable[x][d][y_idx] += lr * \
+            (r + self.discount * max_Q_y - old_score)
+
     def learn(self, rewards, lr={'q': 0.1}):
         for reward in rewards:
             r, info, x, y, d = self._extract(reward)
-            y_idx = self.action_idx[x][y]
-            old_score = self.Qtable[x][d][y_idx]
-            self.Qtable[x][d][y_idx] += lr['q'] * \
-                (r + self.discount * info['max_Q_y'] - old_score)
+            self._update_qtable(r, x, y, d, info['max_Q_y'], lr['q'])
 
 
 class CQ(Qroute):
     attrs = Qroute.attrs | set(['confidence'])
 
-    def __init__(self, network, decay=0.9, initQ=0):
-        super().__init__(network, initQ)
+    def __init__(self, network, decay=0.9, initQ=0, discount=0.9):
+        super().__init__(network, initQ, discount=discount)
         self.decay = decay
         self.confidence = {x: np.zeros_like(
             self.Qtable[x]) for x in self.Qtable.keys()}
@@ -65,18 +68,21 @@ class CQ(Qroute):
             'C_f': self.confidence[action][packet.dest][z_idx]
         }
 
+    def _update_qtable(self, r, x, y, d, C, max_Q):
+        y_idx = self.action_idx[x][y]
+        old_Q = self.Qtable[x][d][y_idx]
+        old_conf = self.confidence[x][d][y_idx]
+        eta = max(C, 1-old_conf)
+        self.Qtable[x][d][y_idx] += eta * \
+            (r + self.discount * max_Q - old_Q)
+        self.confidence[x][d][y_idx] += eta * (C-old_conf)
+        # counteract the effect of confidence_decay()
+        self.confidence[x][d][y_idx] /= self.decay
+
     def learn(self, rewards, lr={}):
         for reward in rewards:
             r, info, x, y, d = self._extract(reward)
-            y_idx = self.action_idx[x][y]
-            old_Q = self.Qtable[x][d][y_idx]
-            old_conf = self.confidence[x][d][y_idx]
-            eta = max(info['C_f'], 1-old_conf)
-            self.Qtable[x][d][y_idx] += eta * \
-                (r + info['max_Q_f'] - old_Q)
-            self.confidence[x][d][y_idx] += eta * (info['C_f']-old_conf)
-            # counteract the effect of confidence_decay()
-            self.confidence[x][d][y_idx] /= self.decay
+            self._update_qtable(r, x, y, d, info['C_f'], info['max_Q_f'])
         self.confidence_decay()
 
     def confidence_decay(self):
@@ -100,26 +106,12 @@ class CDRQ(CQ):
     def learn(self, rewards, lr={}):
         for reward in rewards:
             r_f, info, x, y, dest = self._extract(reward)
-            x_idx, y_idx = self.action_idx[y][x], self.action_idx[x][y]
             " forward "
-            old_Q_f = self.Qtable[x][dest][y_idx]
-            old_conf_f = self.confidence[x][dest][y_idx]
-            eta_f = max(info['C_f'], 1-old_conf_f)
-            self.Qtable[x][dest][y_idx] += eta_f * \
-                (r_f + info['max_Q_f'] - old_Q_f)
-            self.confidence[x][dest][y_idx] += eta_f * \
-                (info['C_f']-old_conf_f)
-            self.confidence[x][dest][y_idx] /= self.decay
+            self._update_qtable(r_f, x, y, dest, info['C_f'], info['max_Q_f'])
             " backward "
-            source = reward.packet.source
             r_b = -info['q_x']-info['t_x']
-            old_Q_b = self.Qtable[y][source][x_idx]
-            old_conf_b = self.confidence[y][source][x_idx]
-            eta_b = max(info['C_b'], 1-old_conf_b)
-            self.Qtable[y][source][x_idx] += eta_b * \
-                (r_b + info['max_Q_b'] - old_Q_b)
-            self.confidence[y][source][x_idx] += eta_b * \
-                (info['C_b']-old_conf_b)
-            self.confidence[y][source][x_idx] /= self.decay
+            source = reward.packet.source
+            self._update_qtable(r_b, y, x, source,
+                                info['C_b'], info['max_Q_b'])
 
         self.confidence_decay()
